f41e13132b38c92c85d5f0a47bfb8873221384c7
tobi <noggnzzz@gmail.com>
2022-05-10 19:22:23 -0500

    Update pyscripts

---
+++ 6to/programacion/psycopg2/departamentosSHP2PSQL.py
@@ -0,0 +1,13 @@
+import geopandas
+from sqlalchemy import create_engine
+
+#Lectura del archivo shape por geopandas
+fileinput = '/mnt/d/tobisthings/sencico/progrmacion/shp/departamentos.shp'
+shp = geopandas.read_file(fileinput)
+# Variables para la conexión
+user = 'postgres'
+password = 'postgres'
+database = 'demo'
+db_connection_url = 'postgresql://'+user+':'+password+'@localhost:5432/'+database
+engine = create_engine(db_connection_url)
+shp.to_postgis(schema='dbcarto_fundamento', name='departamentos', con=engine)
