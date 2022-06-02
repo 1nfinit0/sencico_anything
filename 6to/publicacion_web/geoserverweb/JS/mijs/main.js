d5f731210769446d51582dacd5284c6845f5a720
tobi <noggnzzz@gmail.com>
2022-06-01 21:50:54 -0500

    Comenzado Investigación

--- 6to/publicacion_web/geoserverweb/JS/mijs/main.js
+++ 6to/publicacion_web/geoserverweb/JS/mijs/main.js
@@ -2,7 +2,7 @@ var boundary = [-81.3899688720703, -18.4412956237793, -68.5886001586914, 0.02985
 
 var map = L.map('map', {
     center: [(boundary[1] + boundary[3]) / 2, (boundary[0] + boundary[2]) / 2],
-    zoom: 6
+    zoom: 5
 });
 
 var defaultBase = L.tileLayer.provider('OpenStreetMap').addTo(map);
@@ -27,11 +27,69 @@ var layer002 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_00_0
     transparent: true,
     tiled: 'true'
 });
-
+var layer003 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_03_04/wms', {
+    layers: 'g_03_04:03_04_001_03_001_513_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer004 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_11_01/wms', {
+    layers: 'g_11_01:11_01_001_03_001_531_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer005 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_11_02/wms', {
+    layers: 'g_11_02:11_02_001_03_001_531_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer006 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_11_03/wms', {
+    layers: 'g_11_03:11_03_001_03_001_531_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer007 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_11_04/wms', {
+    layers: 'g_11_04:11_04_001_03_001_531_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer008 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_11_05/wms', {
+    layers: 'g_11_05:11_05_001_03_001_531_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer009 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_11_06/wms', {
+    layers: 'g_11_06:11_06_001_03_001_531_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
+var layer010 = L.tileLayer.wms('http://idesep.senamhi.gob.pe:80/geoserver/g_00_06/wms', 
+    {
+    layers: 'g_00_06:00_06_001_03_000_000_0000_00_00',
+    format: 'image/png',
+    transparent: true,
+    tiled: 'true'
+});
 var groupOverLays = {
-    "Capas": {
+    "Mapa Base": {
         "Departamentos ": layer001,
-        "Provincias"    : layer002
+        "Provincias"    : layer002,
+        "Radiación UV"  : layer003,
+        "Riesgo de Arroz" : layer004,
+        "Riesgo de Maiz" : layer005,
+        "Riesgo de Papa" : layer006,
+        "Riesgo de Cacao" : layer007,
+        "Riesgo de Safé" : layer008,
+        "Riesgo de Pasto" : layer009
+    },
+    "Cuencas": {
+        "Cuencas"         : layer010
     }
 };
 
